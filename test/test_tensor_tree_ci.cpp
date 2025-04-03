12f25672d39211724dde08434b8517e2629bd13f
Thomas Kloss <kloss@itp.uni-frankfurt.de>
2025-04-03 16:28:57 +0200

    add test for more complicated function and many random pivot vectors

--- test/test_tensor_tree_ci.cpp
+++ test/test_tensor_tree_ci.cpp
@@ -33,6 +33,41 @@ TEST_CASE( "Test tensor tree" )
         assert ( abs(ci.tt.eval(grid.coord_to_id(x)) - func(x)) <= 1e-5 );
     }
 
+    SECTION( "random piv" )
+    {
+        srand(0);
+        int num_rand_vec = 20; // number of random global vectors
+
+        int nBit = 25;
+        int dim=3;
+        grid::Quantics grid(0., 1., nBit, dim);
+
+        function func=[&](vector<double> const& x) {
+            auto sum=accumulate(x.begin(), x.end(), 0.0);
+            return cos(sum) + 0.5 * cos(4*x[0] + x[1]) + 0.2 * sin(sum * sum);};
+        function tfunc = [&](vector<int> xi){ return func(grid.id_to_coord(xi));};
+
+        auto tree = makeTuckerTree(dim, nBit);
+
+        vector<vector<int>> pivots;
+        for(auto i=0; i<num_rand_vec; i++) {
+            vector<int> random_vec;
+            for (auto j=0; j<grid.tensorLen; j++) {
+                int randomBit = rand() % 2;
+                random_vec.push_back(randomBit);
+            }
+            pivots.push_back(random_vec);
+        }
+
+        auto ci=TensorTreeCI<double>(tfunc, tree, grid.tensorDims(), {.pivot1=vector(grid.tensorLen, 0)});
+
+        ci.addPivotsAllBonds(pivots);
+        ci.iterate(1,0);
+
+        vector<double> x = {0.3, 0.2, 0.7};
+        assert ( abs(ci.tt.eval(grid.coord_to_id(x)) - func(x)) <= 1e-5 );
+    }
+
     SECTION( "cos" )
     {
         int nBit = 25;
